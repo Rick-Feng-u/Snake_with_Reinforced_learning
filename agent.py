bbbaf5c9dbd313b6b3c8534f29f2c28789aca941
Rick Feng <rickfeng99@gmail.com>
2021-02-19 18:22:35 -0800

    getState function and remember function is completed

--- agent.py
+++ agent.py
@@ -15,19 +15,64 @@ class Agent:
         self.epsilon = 0 # randomness
         self.gamma = 0 # discount rate
         self.memeory = deque(maxlen = MAX_MEMORY) # popleft() if its exceed the memeory limit
+        self.model = None # TODO :v
+        self.trainer # TODO :v
         # TODO: model, trainer
 
     def getState(self, game):
-        pass
+        head = game.snake[0]
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y + 20)
+        
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        state = [
+            # Danger straight
+            (dir_r and game.is_collision(point_r)) or 
+            (dir_l and game.is_collision(point_l)) or 
+            (dir_u and game.is_collision(point_u)) or 
+            (dir_d and game.is_collision(point_d)),
+
+            # Danger right
+            (dir_u and game.is_collision(point_r)) or 
+            (dir_d and game.is_collision(point_l)) or 
+            (dir_l and game.is_collision(point_u)) or 
+            (dir_r and game.is_collision(point_d)),
+
+            # Danger left
+            (dir_d and game.is_collision(point_r)) or 
+            (dir_u and game.is_collision(point_l)) or 
+            (dir_r and game.is_collision(point_u)) or 
+            (dir_l and game.is_collision(point_d)),
+            
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+            
+            # Food location 
+            game.food.x < game.head.x,  # food left
+            game.food.x > game.head.x,  # food right
+            game.food.y < game.head.y,  # food up
+            game.food.y > game.head.y  # food down
+            ]
+
+        return np.array(state, dtype=int) # so bascially turning true = 1 and false = 0 (very big brain)
 
     def remember(self, state, action, reward, nextState, game_over):
-        pass
+        self.memeory.append(state, action, reward, nextState, game_over)
 
     def train_long_memeory(self):
         pass
 
     def train_short_memeory(self, state, action, reward, nextState, game_over): # one step
-        pass
+        self
 
     def getAction(self,state):
         pass
